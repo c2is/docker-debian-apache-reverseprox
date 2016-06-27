92fa3c7e7b8a08e0c8e5edd55b248dd1340f063d
a.cianfarani <a.cianfarani@c2is.fr>
2016-06-27 16:00:24 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# C2is container for Apache SSL Reverse Proxy  
+
+### Usage  
+
+##### With docker-compose (use image already built from docker's hub)
+```
+# In your docker-compose.yml file
+reverseproxy:
+    build: c2is/a2-reverseprox
+    ports:
+        - "443:443"
+    environment:
+        - PROXY_TARGET=192.168.99.100:82
+        - CERTIFICAT_CNAME=www.example.com
+```
+
---
+++ entrypoint.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+set -e
+
+
+if [ -z "$PROXY_TARGET" ]; then
+  PROXY_TARGET="192.168.99.100:80"
+fi
+if [ -z "$CERTIFICAT_CNAME" ]; then
+  CERTIFICAT_CNAME="www.exemple.com"
+fi
+
+cat <<EOF > /etc/apache2/sites-available/proxy-reverse-ssl.conf
+<VirtualHost *:443>
+        ServerAdmin webmaster@localhost
+
+        ErrorLog ${APACHE_LOG_DIR}/error-ssl.log
+        CustomLog ${APACHE_LOG_DIR}/access-ssl.log combined
+
+        SSLEngine on
+
+        SSLCertificateFile    /etc/apache2/ssl/proxy.crt
+        SSLCertificateKeyFile /etc/apache2/ssl/proxy.key
+
+        BrowserMatch "MSIE [2-6]" \
+                nokeepalive ssl-unclean-shutdown \
+                downgrade-1.0 force-response-1.0
+        BrowserMatch "MSIE [17-9]" ssl-unclean-shutdown
+
+        RewriteEngine on
+        ProxyPreserveHost On
+        ProxyRequests off
+        ProxyPassReverse / http://$PROXY_TARGET/
+        RewriteRule ^/(.*) http://$PROXY_TARGET/\$1 [P,L]
+
+</VirtualHost>
+EOF
+
+/bin/ln -sf /etc/apache2/sites-available/proxy-reverse-ssl.conf /etc/apache2/sites-enabled/001-proxy-reverse-ssl.conf
+
+openssl req -new -newkey rsa:4096 -days 365 -nodes -x509 -subj "/C=FR/ST=c2is/L=Lyon/O=c2is/CN=$CERTIFICAT_CNAME" -keyout /etc/apache2/ssl/proxy.key -out /etc/apache2/ssl/proxy.crt
+
+exec "$@"
+
